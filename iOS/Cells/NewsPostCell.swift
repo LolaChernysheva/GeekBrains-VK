acffbd6623b14d4f2b20caa582c5e254ab5f335f
LolaChernysheva <lolita-chernysheva@mail.ru>
2020-06-21 22:39:28 +0300

    Добавлен прототип ячейки новостей типа  Post

---
+++ iOS/Cells/NewsPostCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsPostCell.swift
+//  iOS
+//
+//  Created by Лолита on 21.06.2020.
+//  Copyright © 2020 Лолита Чернышева. All rights reserved.
+//
+
+import UIKit
+
+class NewsPostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
